646744acc879ec43600173324f128b428b040e48
hrsprs <jamescharleson@yahoo.com>
2022-12-05 11:40:06 -0700

    set up connections and index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const db = require('./config/connection');
+const routes = require('./routes');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Now Listening on Port ${PORT}`);
+    });
+});
