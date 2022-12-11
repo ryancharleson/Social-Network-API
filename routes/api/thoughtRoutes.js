05648eb7fd4278a2711dd098e3909b1cd128707f
hrsprs <jamescharleson@yahoo.com>
2022-12-11 13:10:48 -0700

    added routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThought).post(createThought);
+router.route('/:id').get(getSingleThought).delete(deleteThought).put(updateThought);
+router.route('/:thoughtId/reaction').post(createReaction);
+router.route('/:thoughtId/reaction/:reactionId').delete(deleteReaction)
+
+module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
-
 const {
-    createUser,
     getUsers,
-    updateUser,
     getSingleUser,
+    createUser,
+    updateUser,
     deleteUser,
     addFriend,
     deleteFriend,
 } = require('../../controllers/userController');
 
-
 router.route('/').get(getUsers).post(createUser);
+
 router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
